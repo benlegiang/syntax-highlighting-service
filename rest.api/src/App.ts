8a9eb2f3f8711588431e3d44bb6a27b19661dae0
Hoàng Ben Lê Giang <hoangben.legiang@rhyno.ch>
2022-07-13 19:58:32 +0200

    feat: init rest.api

---
+++ rest.api/src/App.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { Rest } from "./routes/v1/rest";
+
+class App {
+  public express: express.Express;
+
+  constructor() {
+    this.express = express();
+    this.express.use(express.json());
+    this.mountRoutes();
+  }
+
+  private mountRoutes(): void {
+    const router = express.Router();
+
+    router.get("/", (req, res) => {
+      res.json({ message: "syntax highlighting api" });
+    });
+    this.express.use("/", router);
+    this.express.use("api/v1", new Rest().getRouter());
+  }
+}
+
+export default new App().express;
