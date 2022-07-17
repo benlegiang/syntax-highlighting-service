0c934eac59bf6991843b5df8097e852f12a2c27d
Hoàng Ben Lê Giang <hoangben.legiang@uzh.ch>
2022-07-17 23:51:21 +0200

    fix: db config changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-git clone https://github.com/benlegiang/syntax-highlighting-service.git
-cd syntax-highlighting-service/
 docker compose build
 docker compose push
 docker compose down
