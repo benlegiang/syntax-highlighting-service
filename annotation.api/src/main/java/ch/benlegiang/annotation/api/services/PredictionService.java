2b90dc2f41672f29d274acc3f4a744b992ce831e
Hoàng Ben Lê Giang <hoangben.legiang@uzh.ch>
2022-07-31 23:29:01 +0200

    fix: trigger workflow

--- annotation.api/src/main/java/ch/benlegiang/annotation/api/services/PredictionService.java
+++ annotation.api/src/main/java/ch/benlegiang/annotation/api/services/PredictionService.java
@@ -1,8 +1,6 @@
 package ch.benlegiang.annotation.api.services;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
