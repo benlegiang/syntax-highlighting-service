59d84a2cabb03409887f0f080689cca4db4e8fa0
Hoàng Ben Lê Giang <hoangben.legiang@uzh.ch>
2022-08-02 15:36:40 +0200

    feat: trigger docker image publish

--- training.api/app/main.py
+++ training.api/app/main.py
@@ -7,5 +7,5 @@ def index():
     return jsonify({'message':'syntax highlighting training api'})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8000)
+    app.run(host='0.0.0.0', debug=True, port=8001)
 
