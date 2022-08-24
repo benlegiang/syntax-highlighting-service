d5409aaef6bf3c0bb0e8ab78fcfaaa2713527e61
Hoàng Ben Lê Giang <hoangben.legiang@uzh.ch>
2022-08-24 20:05:58 +0200

    feat: continuous model build

--- training.api/app/main.py
+++ training.api/app/main.py
@@ -1,35 +1,47 @@
-from crypt import methods
 from flask import Flask, jsonify, request
 
 from app.utils.SHModelUtils import *
 from app.utils.services.ModelService import ModelService
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+prediction_api = 'http://syntax-highlighting-service-prediction-api:8000/api/v1'
 database: str = 'syntaxHighlighting'
 annotations_collection: str = 'annotations'
 batch_size: int = 20000
 training_size: float = 0.8
+check_db_interval: int = 30 # in seconds for now
+threshold = 3
+
+modelService = ModelService(prediction_api, database, annotations_collection, batch_size, training_size)
+
+# Scheduler for checking changes to DB
+
+scheduler = BackgroundScheduler()
+# job_defaults = {
+#     'coalesce': False,
+#     'max_instances': 1
+# }
+scheduler.add_job(modelService.check_db_changes, 'interval', seconds=check_db_interval, args=(database, annotations_collection, threshold))
+scheduler.start()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return jsonify({'message':'syntax highlighting training api'})
+    
 
-@app.route("/init", methods=['POST'])
-def init_model_training():
+# Endpoint is only called once to initiate empty models
+@app.route("/api/v1/build", methods=['POST'])
+def build_model():
     model_lang: str = request.args.get('lang').upper()
 
     try:
-        modelService = ModelService(database, annotations_collection, model_lang, batch_size, training_size)
-        return jsonify({'success': True, 'modelLang': model_lang, 'modelNumber': modelService.model_number})
+        model_number = modelService.build_model(model_lang)
+        return jsonify({'success': True, 'modelLang': model_lang, 'modelNumber': model_number})
 
     except Exception as e: 
         return jsonify({'success': False, 'message': e })
-
-
-@app.route("/build", methods=['POST'])
-def build():
-
     pass
 
 if __name__ == "__main__":
--- training.api/app/utils/services/ModelService.py
+++ training.api/app/utils/services/ModelService.py
@@ -1,74 +1,86 @@
+from lib2to3.pygram import python_symbols
 import pickle
 from datetime import datetime
 import random
+import time
 from app.utils.SHModelUtils import JAVA_LANG_NAME, KOTLIN_LANG_NAME, PYTHON3_LANG_NAME, SHModel
 from app.utils.services.MongoDatabase import MongoDatabase
+import logging
+import requests
+
+logging.basicConfig(filename="logs.txt")
 
 class ModelService:
 
-    def __init__(self, database: str, collection: str, model_lang: str, batch_size: int, training_size: float):
+    def __init__(self, prediction_api: str, database: str, collection: str, batch_size: int, training_size: float):
+        self.client = MongoDatabase().client
+        self.prediction_api = prediction_api
         self.database = database
         self.collection = collection
         self.batch_size = batch_size
         self.training_size = training_size
-        self.model_number = 0
+        
+        self.python3_size = 0
+        self.java_size = 0
+        self.kotlin_size = 0
 
-        self.setup_model(model_lang)
+    def build_deploy_model(self,model_lang: str):
+        model_number = self.build_model(model_lang)
+        self.trigger_model_pull(model_lang, model_number)
 
-    def setup_model(self, model_lang: str):
+    def trigger_model_pull(self, model_lang: str, model_number: int):
+        requests.post(self.prediction_api + f'/deploy?lang={model_lang}&no={model_number}')
 
+    def build_model(self, model_lang: str):
         if model_lang == 'PYTHON3':
-            self.model_number = self.get_model_number(model_lang)
-            print(self.model_number)
-
-            model = SHModel(PYTHON3_LANG_NAME, f'model_{self.model_number}')
+            model_number = self.get_latest_model_number(model_lang) + 1
+            model = SHModel(PYTHON3_LANG_NAME, f'model_{model_number}')
             model.setup_for_finetuning()
             # Skip fine-tuning of very first model
-            if self.model_number != 0:
-                self.fine_tune_model(model, self.model_number, model_lang)
+            if model_number != 1:
+                self.fine_tune_model(model, model_number, model_lang)
             else:
-                self.save_model_to_db(model, self.model_number, model_lang, 0, 0, 0, 100)
+                self.save_model_to_db(model, model_number, model_lang, 0, 0, 0, 100)
+
+            return model_number
 
         elif model_lang == 'JAVA':
-            self.model_number = self.get_model_number(model_lang)
-            print(self.model_number)
-            model = SHModel(JAVA_LANG_NAME, f'model_{self.model_number}')
+            model_number = self.get_latest_model_number(model_lang) + 1
+            model = SHModel(JAVA_LANG_NAME, f'model_{model_number}')
             model.setup_for_finetuning()
-            if self.model_number != 0:
-                self.fine_tune_model(model, self.model_number, model_lang)
+            if model_number != 1:
+                self.fine_tune_model(model, model_number, model_lang)
             else:
-                self.save_model_to_db(model, self.model_number, model_lang, 0, 0, 0, 100)
+                self.save_model_to_db(model, model_number, model_lang, 0, 0, 0, 100)
+            
+            return model_number
+
         
         elif model_lang == 'KOTLIN':
-            self.model_number = self.get_model_number(model_lang)
-            print(self.model_number)
-
-            model = SHModel(KOTLIN_LANG_NAME, f'model_{self.model_number}')
+            model_number = self.get_latest_model_number(model_lang) + 1
+            model = SHModel(KOTLIN_LANG_NAME, f'model_{model_number}')
             model.setup_for_finetuning()
-            if self.model_number != 0:
-                self.fine_tune_model(model, self.model_number, model_lang)
+            if model_number != 1:
+                self.fine_tune_model(model, model_number, model_lang)
             else:
-                self.save_model_to_db(model, self.model_number, model_lang, 0, 0, 0, 100)
+                self.save_model_to_db(model, model_number, model_lang, 0, 0, 0, 100)
+            return model_number
 
-    def get_model_number(self, model_lang: str):
-        client = MongoDatabase().client
-
-        db = client[self.database]
+    def get_latest_model_number(self, model_lang: str):
+        db = self.client[self.database]
 
         collection = db[model_lang]
 
         size = collection.count_documents({})
 
-        return size + 1
+        return size
 
     def save_model_to_db(self, model: SHModel, model_number: int, model_lang: str, training_size: int, test_size: int, accuracy: float, loss: float):
-        client = MongoDatabase().client
-
         model.persist_model()
         try:
             pickled_model = pickle.dumps(model)
 
-            db = client[self.database]
+            db = self.client[self.database]
             collection = db[model_lang]
 
             query = collection.insert_one({"modelNumber": model_number, "modelData": pickled_model, "trainingSize": training_size, "testSize": test_size, "accuracy": accuracy, "loss": loss, "createdAt": datetime.now().isoformat()})
@@ -77,16 +89,14 @@ class ModelService:
 
             return details
         except Exception as e: 
-            print(e)
+            logging.error(e)
             return 'Saving model to database failed'
 
 
     def load_annotations_from_db(self, code_lang):
 
-        client = MongoDatabase().client
-
         try:
-            db = client[self.database]
+            db = self.client[self.database]
             collection = db[self.collection]
 
             annotations_query = collection.aggregate([
@@ -109,7 +119,7 @@ class ModelService:
             return annotations
 
         except Exception as e:
-            print(e)
+            logging.error(e)
             return 'Loading annotations from database failed'
 
 
@@ -118,12 +128,17 @@ class ModelService:
         try: 
             annotations = self.load_annotations_from_db(model_lang)
 
-            loss = 0
+            train_data = annotations[:int((len(annotations)+1)*self.training_size)]
+            test_data = annotations[int((len(annotations)+1)*self.training_size):]
+
+            loss = None
+            accuracy = None
+            training_size = len(train_data)
+            test_size = len(test_data)
+            
 
             random.shuffle(annotations)
 
-            train_data = annotations[:int((len(annotations)+1)*self.training_size)]
-            test_data = annotations[int((len(annotations)+1)*self.training_size):]
 
             for annotation in train_data:
                 loss = model.finetune_on(annotation['tokenIds'], annotation['formal'])
@@ -133,21 +148,17 @@ class ModelService:
             # Set up for prediction to measure accuracy of fine-tuning
             model.setup_for_prediction()
             
-            total_items = len(test_data)
             correct = 0
-            print("Predicting...")
-
             accuracy = None
+
             for annotation in test_data:
                 p = model.predict(annotation['tokenIds'])
                 if p == annotation['formal']:
                     correct += 1
 
-                accuracy = correct / total_items * 100
+                accuracy = correct / test_size * 100
 
-                client = MongoDatabase().client
-
-                db = client[self.database]
+                db = self.client[self.database]
                 collection = db[self.collection]
 
                 query = {'_id': {'$in': test_ids}}
@@ -155,10 +166,41 @@ class ModelService:
 
                 collection.update_many(query, update)
 
-            self.save_model_to_db(model, model_number, model_lang, len(train_data), len(test_data), accuracy, loss)
+            # TODO: Only save to DB if it actually improved!!!!!!!!!!
+            # See ConUpdate/Reuse
+            self.save_model_to_db(model, model_number, model_lang, training_size, test_size, accuracy, loss)
 
         except Exception as e:
-            print(e)
+            logging.error(e)
+
+    def check_db_changes(self, database: str, annotations_collection: str, threshold: int): 
+        db = self.client[database]
+        collection = db[annotations_collection]
+
+        python3_size = collection.count_documents({'codeLanguage': 'PYTHON3', 'isTrainable': True, 'isTestItem': False})
+        java_size = collection.count_documents({'codeLanguage': 'JAVA', 'isTrainable': True, 'isTestItem': False})
+        kotlin_size = collection.count_documents({'codeLanguage': 'KOTLIN', 'isTrainable': True, 'isTestItem': False})
+
+        python3_model_number_latest = self.get_latest_model_number('PYTHON3') 
+        java_model_number_latest = self.get_latest_model_number('JAVA') 
+        kotlin_model_number_latest = self.get_latest_model_number('KOTLIN')
+
+        if python3_model_number_latest > 0:
+            if python3_size - self.python3_size > threshold and python3_size != 0:
+                self.build_model('PYTHON3')
+                # Re-count due to changes caused by splitting data sets
+                self.python3_size = collection.count_documents({'codeLanguage': 'PYTHON3', 'isTrainable': True, 'isTestItem': False})
+        
+        if java_model_number_latest > 0:
+            if java_size - self.java_size > threshold and java_size != 0:
+                self.build_model('JAVA')
+                self.java_size = collection.count_documents({'codeLanguage': 'JAVA', 'isTrainable': True, 'isTestItem': False})
+
+        if kotlin_model_number_latest > 0:
+            if kotlin_size - self.kotlin_size > threshold and kotlin_size != 0:
+                self.build_model('KOTLIN')
+                print("Success: Built Kotlin model")
+                self.kotlin_size = collection.count_documents({'codeLanguage': 'KOTLIN', 'isTrainable': True, 'isTestItem': False})
 
 
 
