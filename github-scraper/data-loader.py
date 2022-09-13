3848b2eb6f9c007f9f1cad54cf16bd387897fc54
Hoàng Ben Lê Giang <hoangben.legiang@uzh.ch>
2022-09-13 23:19:04 +0200

    fix: send seq.

--- github-scraper/data-loader.py
+++ github-scraper/data-loader.py
@@ -1,8 +1,6 @@
 import requests
-from concurrent.futures import ThreadPoolExecutor
 import json
 import sys
-
 
 annotation_api_url = 'http://localhost:8081/api/v1/highlight'
 
@@ -20,12 +18,8 @@ def annotate_files(code_lang, file_name):
     data = [json.loads(line) for line in open(file_name, 'r')]
 
 
-    with ThreadPoolExecutor(max_workers=6) as executor:
-        threads = []
-
-        for i in range(len(data)):
-            threads.append(executor.submit(send_single_entry, code_lang, data[i]['source'], i))
+    for i in range(len(data)):
+        send_single_entry(code_lang, data[i]['source'], i)
 
 if __name__ == '__main__':
     annotate_files(sys.argv[1], sys.argv[2])
-    
