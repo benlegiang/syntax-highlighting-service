5d4bad285246bcb5eae9e57a3985b768f88c7951
Hoàng Ben Lê Giang <55922993+benlegiang@users.noreply.github.com>
2022-09-22 22:37:42 +0200

    Delete deploy.sh

--- rest.api/deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-docker stop syntax-highlighting-service-rest-api
-docker rm syntax-highlighting-service-rest-api
-docker rmi benlegiang/syntax-highlighting-service-rest-api:master
-docker pull benlegiang/syntax-highlighting-service-rest-api:master
-
-docker run -t -d -p syntax-highlighting-service-rest-api benlegiang/syntax-highlighting-service-rest-api:master
